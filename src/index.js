39fed41393f14b7c46dd0b36c060cc0c760bd649
Gabriel Rezende <g@brielrezen.de>
2021-04-05 20:26:32 -0300

    routes adicionado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express()
+const routes = require('./routes/index')
+
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.listen(3000, () => { console.log('servidor rodando')})
 
 
 
-app.listen(3000)
-
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
